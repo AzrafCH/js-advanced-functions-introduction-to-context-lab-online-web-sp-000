8d0b383a1faaae7fc15953fc90a7ad24d041b350
Azraf Chowdhury <achowdhur@ltu.edu>
2020-08-19 06:26:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ function createTimeInEvent(obj, timeStamp) {
   obj.timeInEvents.push({type: "Timein", hour: hour, date: date})
   return obj
 }
+
+function createTimeOutEvent(obj, timeStamp) {
+  let hour = parseInt(timeStamp.split(' ')[1]);
+  let date = timeStamp.split(' ')[0];
+  obj.timeOutEvents.push({type: "Timeout", hour: hour, date: date})
+  return obj 
+}
