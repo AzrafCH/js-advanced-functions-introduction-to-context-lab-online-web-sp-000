fef0bbe151145476e5019e1e9c3f62a96d232fa4
Azraf Chowdhury <achowdhur@ltu.edu>
2020-08-19 06:15:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function createEmployeeRecord(arr) {
     firstName: arr[0],
     familyName: arr[1],
     title: arr[2],
-    payPerHour: arr[4],
+    payPerHour: arr[3],
     timeInEvents: [],
     timeOutEvents: []
   }
