a41fec9b5bc604123e9ac65cb8f1e78611dbd1a4
Azraf Chowdhury <achowdhur@ltu.edu>
2020-08-19 06:15:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function createEmployeeRecord(arr) {
+  let obj = {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[4],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return obj 
+}
